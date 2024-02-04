7144088a1f8ff51df7f51b43b71ddce8a0e1c21e
hwk <hwk1727370054@126.com>
2024-02-05 04:54:06 +0800

    feature/ 完成了root用户添加普通用户的功能

---
+++ src/disk_client_gui/add_user.cpp
@@ -0,0 +1,17 @@
+﻿#include "add_user.h"
+
+AddUser::AddUser(QWidget *parent)
+    : QDialog(parent)
+{
+    ui.setupUi(this);
+}
+
+AddUser::~AddUser()
+{
+}
+void AddUser::accept()
+{
+    password = ui.passwordEdit->text().toLocal8Bit();
+    username = ui.userEdit->text().toLocal8Bit();
+    QDialog::accept();
+}
---
+++ src/disk_client_gui/add_user.h
@@ -0,0 +1,23 @@
+﻿#ifndef ADD_USER_H
+#define ADD_USER_H
+
+#include <QDialog>
+#include "ui_add_user.h"
+#include<string>
+
+class AddUser : public QDialog
+{
+    Q_OBJECT
+
+public:
+    AddUser(QWidget *parent = Q_NULLPTR);
+    ~AddUser();
+    void AddUser::accept() override;
+    std::string password = "";
+    std::string username="";
+
+private:
+    Ui::AddUser ui;
+};
+
+#endif // ADD_USER_H
