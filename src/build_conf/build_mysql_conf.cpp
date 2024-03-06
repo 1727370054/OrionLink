84e2d67efcf451267af28d4b93a80d91e5c96257
hwk <hwk1727370054@126.com>
2024-03-06 22:48:24 +0800

    Update 输入密码时不回显

--- src/build_conf/build_mysql_conf.cpp
+++ src/build_conf/build_mysql_conf.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
+#include <termio.h>
 
 using namespace std;
 
@@ -14,16 +16,57 @@ struct MySQLInfo
     int port = 3306;
 };
 
+char _getch()
+{
+    termios new_tm;
+    /// 原来的模式
+    termios old_tm;
+    int fd = 0;
+    if (tcgetattr(fd, &old_tm) < 0)
+        return -1;
+    /// 更改为原始模式，没有回显
+    cfmakeraw(&new_tm);
+    if (tcsetattr(fd, TCSANOW, &new_tm) < 0)
+    {
+        return -1;
+    }
+    char c = getchar();
+    if (tcsetattr(fd, TCSANOW, &old_tm) < 0)
+    {
+        return -1;
+    }
+
+    return c;
+}
+
+std::string InputPassword()
+{
+    /// 清空缓冲
+    cin.ignore(4096, '\n');
+    string password = "";
+    for (;;)
+    {
+        char a = _getch(); /// 获取输入字符不显示
+        if (a <= 0 || a == '\n' || a == '\r') break;
+        password += a;
+        cout << "*" << flush;
+    }
+
+    return password;
+}
+
 int main(int argc, char *argv[])
 {
     MySQLInfo info;
-    cout << "input the db set" << endl;
+    cout << "======= input the db set =======" << endl;
     cout << "input db host:";
     cin >> info.host;
     cout << "input db user:";
     cin >> info.user;
     cout << "input db pass:";
-    cin >> info.pass;
+    std::string password = InputPassword();
+	strncpy(info.pass, password.c_str(), sizeof(info.pass) - 1);
+	info.pass[sizeof(info.pass) - 1] = '\0';
     cout << endl;
     cout << "input db dbname(orion_link):";
     cin >> info.db_name;
